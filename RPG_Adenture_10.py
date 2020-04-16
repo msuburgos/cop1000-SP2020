aeffc6244100bf875a44dee6d77a29da1d2bdbbd
homer72 <51171262+homer72@users.noreply.github.com>
2020-04-16 14:49:50 -0400

    Update RPG_Adenture_10.py

--- RPG_Adenture_10.py
+++ RPG_Adenture_10.py
@@ -1,24 +1,24 @@
 import os
 import time
 # Nacho_World
-def__init__('go class', 'spicy_nachos')
-  macho_picy= Yes
-  mild= maybe
-  no_spicy= no
-print('hot_spicy', 'mild','no_spicy)
+def__init__(self)
+  macho_picy= 'yes'
+  mild= 'maybe'
+  no_spicy= 'no'
+  print('hot_spicy', 'mild','no_spicy')
 # direction
-def _str_(direction to waterfall)
-if left is orange_waterfall, Up yellow__waterfall, down_brown_waterfall,then right tan_waterfall
-Print(‘left_orange_waterfall’, ‘Up yellow__waterfall’, ‘ down_brown_waterfall’, ’right tan_waterfall’)
+def _str_(direction)
+  if direction == 'orange_waterfall' or direction ==  'yellow__waterfall' or direction == 'brown_waterfall' or direction ==  'right tan_waterfall':
+    print(‘left_orange_waterfall’, ‘Up yellow__waterfall’, ‘ down_brown_waterfall’, ’right tan_waterfall’)
 
-# Nacho_meat_ attack
-if you come= _meat attack
-miniature_meat_attack=strings('chicken','ground_beef',veggies')
-print('meat_attack)
-#Topping Splash on you if fail 
-If_topping=="volcano splash bowl"
-Splash_Bowl_Ingredients= strings('black_olives', 'tomatoes','jalapeño', 'cucumber',' 'onions', 'lettuce', 'spinach', 'black_beans','sour cream', 'queso blanco')
-print(Splash_Bowl_Ingredients')
+  # Nacho_meat_ attack
+  if you come == _meat attack
+    miniature_meat_attack=strings('chicken','ground_beef',veggies')
+  print('meat_attack)
+  #Topping Splash on you if fail 
+  If_topping=="volcano splash bowl"
+  Splash_Bowl_Ingredients= strings('black_olives', 'tomatoes','jalapeño', 'cucumber',' 'onions', 'lettuce', 'spinach', 'black_beans','sour cream', 'queso blanco')
+  print(Splash_Bowl_Ingredients')
 
 
 #after the end of the world
